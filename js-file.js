3cbcd84c125aa132b572f748056d2e7ee6aba39e
Matthew McAuley <mattwilliam618@gmail.com>
2023-11-05 22:14:40 -0500

    Fixed variable name
    Used wrong variable name leading to error being thrown

--- js-file.js
+++ js-file.js
@@ -2,7 +2,7 @@ const squareContainer = document.querySelector("#squares");
 for (let row = 0; row < 15; row++) {
     let rowDiv = document.createElement("div")
     rowDiv.setAttribute("id", "rowDiv")
-    squareContainer.appendChild(rowDivs)
+    squareContainer.appendChild(rowDiv)
     for (let col = 0; col < 15; col++) {
         let square = document.createElement("div");
         rowDiv.appendChild(square)
