8973d1aa39021f2fd1f15cde895222e4c75b7548
Matthew McAuley <mattwilliam618@gmail.com>
2023-11-06 00:53:12 -0500

    Added button to change grid dimension
    Added button with click event to replace old grid with new one of
    specified dimensions between 0,100 inclusive

--- js-file.js
+++ js-file.js
@@ -1,17 +1,34 @@
 //Grid creation
 const squareContainer = document.querySelector("#squares");
-for (let row = 0; row < 15; row++) {
-    let rowDiv = document.createElement("div")
-    rowDiv.setAttribute("id", "rowDiv")
-    squareContainer.appendChild(rowDiv)
-    for (let col = 0; col < 15; col++) {
-        let square = document.createElement("div");
-        rowDiv.appendChild(square)
-        square.setAttribute("id", "square")
-        square.addEventListener('mouseover', function(event) {
-            const source = event.target;
-            source.style.backgroundColor = "blue";
-            console.log("happened");
-        });
+function createGrid(dimension) {
+    for (let row = 0; row < dimension; row++) {
+        let rowDiv = document.createElement("div")
+        rowDiv.setAttribute("id", "rowDiv")
+        squareContainer.appendChild(rowDiv)
+        for (let col = 0; col < dimension; col++) {
+            let square = document.createElement("div");
+            rowDiv.appendChild(square)
+            square.setAttribute("id", "square")
+            //change square colors when hovered over
+            square.addEventListener('mouseover', function(event) {
+                const source = event.target;
+                source.style.backgroundColor = "blue";
+                console.log("happened");
+            });
+        }
     }
 }
+
+const dimensionButton = document.querySelector("button");
+dimensionButton.addEventListener('click', () => {
+    let val = Number(prompt("New Dimension [0,100]: "));
+    while (typeof(val) != "number" || val < 0 || val > 100) {
+        console.log(typeof(val));
+        console(val);
+        val = prompt("Please enter a valid number [0,100]: ");
+    }
+    while (squareContainer.firstChild) {
+        squareContainer.firstChild.remove()
+    }
+    createGrid(val);
+});
