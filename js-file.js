ddabf44f0b1c734ce63a8435ac0b943acefaf095
Matthew McAuley <mattwilliam618@gmail.com>
2023-11-07 21:52:01 -0500

    Added slider for dimension change and edited appearance
    Wanted fluid slider for dimension change instead of alert
    button and made things more appealing

--- js-file.js
+++ js-file.js
@@ -46,18 +46,17 @@ function createGrid(dimension) {
     }
 }
 
-let val;
-const dimensionButton = document.querySelector("#dimension");
-dimensionButton.addEventListener('click', () => {
-    val = Number(prompt("New Dimension [1,100]: "));
-    while (isNaN(val) || val < 1 || val > 100) {
-        val = Number(prompt("Please enter a valid number [1,100]: "));
-    }
-    while (squareContainer.firstChild) {
-        squareContainer.firstChild.remove()
-    }
-    createGrid(val);
-});
+// const dimensionButton = document.querySelector("#dimension");
+// dimensionButton.addEventListener('click', () => {
+//     let val = Number(prompt("New Dimension [1,100]: "));
+//     while (isNaN(val) || val < 1 || val > 100) {
+//         val = Number(prompt("Please enter a valid number [1,100]: "));
+//     }
+//     while (squareContainer.firstChild) {
+//         squareContainer.firstChild.remove()
+//     }
+//     createGrid(val);
+// });
 
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener('click', () => {
@@ -66,3 +65,17 @@ clearButton.addEventListener('click', () => {
         squares[i].style.backgroundColor = "white";
     }
 });
+
+const slider = document.querySelector(".slider");
+const dimensionValue = document.querySelector("#slidervalue");
+slider.addEventListener('mouseup', (event) => {
+    let sliderValue = event.target.value;
+    while (squareContainer.firstChild) {
+        squareContainer.firstChild.remove()
+    }
+    createGrid(sliderValue);
+})
+slider.addEventListener('input', (event) => {
+    let sliderValue = event.target.value;
+    dimensionValue.textContent = "Dimension: " + sliderValue;
+})
